3223b3823368d1b4810b2f71544e3fdd1ad30456
a91n0d <a91n0d@gmail.com>
2022-01-12 09:16:57 +0300

    Add project files.

---
+++ CountingStringChars/DoWhileMethods.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+namespace CountingStringChars
+{
+    public static class DoWhileMethods
+    {
+        /// <summary>
+        /// Returns a number of digits in a string.
+        /// </summary>
+        /// <param name="str">A <see cref="string"/> to search.</param>
+        /// <returns>A number of digits in a string.</returns>
+        public static int GetDigitCount(string str)
+        {
+            if (str is null)
+            {
+                throw new ArgumentNullException(nameof(str));
+            }
+
+            int count = 0;
+            int i = 0;
+            do
+            {
+                if (str.Length > 0)
+                {
+                    count += char.IsDigit(str[i]) ? 1 : 0;
+                    i++;
+                }
+            }
+            while (i < str.Length);
+            return count;
+        }
+
+        /// <summary>
+        /// Returns a number of letters in a string.
+        /// </summary>
+        /// <param name="str">A <see cref="string"/> to search.</param>
+        /// <returns>A number of letters in a string.</returns>
+        public static int GetLetterCount(string str)
+        {
+            if (str is null)
+            {
+                throw new ArgumentNullException(nameof(str));
+            }
+
+            int count = 0;
+            int i = 0;
+            do
+            {
+                if (str.Length > 0)
+                {
+                    count += char.IsLetter(str[i]) ? 1 : 0;
+                    i++;
+                }
+            }
+            while (i < str.Length);
+            return count;
+        }
+
+        /// <summary>
+        /// Returns a number of digits in a string.
+        /// </summary>
+        /// <param name="str">A <see cref="string"/> to search.</param>
+        /// <returns>A number of digits in a string.</returns>
+        public static int GetDigitCountRecursive(string str)
+        {
+            if (str is null)
+            {
+                throw new ArgumentNullException(nameof(str));
+            }
+
+            return GetDigitCountRecursive(str, str.Length, 0);
+        }
+
+        /// <summary>
+        /// Returns a number of letters in a string.
+        /// </summary>
+        /// <param name="str">A <see cref="string"/> to search.</param>
+        /// <returns>A number of letters in a string.</returns>
+        public static int GetLetterCountRecursive(string str)
+        {
+            if (str is null)
+            {
+                throw new ArgumentNullException(nameof(str));
+            }
+
+            return GetLetterCountRecursive(str, str.Length, 0);
+        }
+
+        private static int GetDigitCountRecursive(string str, int charsLeft, int counter)
+        {
+            if (charsLeft > 0)
+            {
+                return GetDigitCountRecursive(str, charsLeft - 1, char.IsDigit(str[^charsLeft]) ? ++counter : counter);
+            }
+
+            return counter;
+        }
+
+        private static int GetLetterCountRecursive(string str, int charsLeft, int counter)
+        {
+            if (charsLeft > 0)
+            {
+                return GetLetterCountRecursive(str, charsLeft - 1, char.IsLetter(str[^charsLeft]) ? ++counter : counter);
+            }
+
+            return counter;
+        }
+    }
+}
---
+++ CountingStringChars/WhileMethods.cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+namespace CountingStringChars
+{
+    public static class WhileMethods
+    {
+        /// <summary>
+        /// Returns a number of white space characters in a string.
+        /// </summary>
+        /// <param name="str">A <see cref="string"/> to search.</param>
+        /// <returns>A number of white space characters in a string.</returns>
+        public static int GetSpaceCount(string str)
+        {
+            if (str is null)
+            {
+                throw new ArgumentNullException(nameof(str));
+            }
+
+            if (string.IsNullOrEmpty(str))
+            {
+                return 0;
+            }
+
+            int result = 0;
+            int i = 0;
+            while (i <= str.Length - 1)
+            {
+                result += char.IsWhiteSpace(str[i]) ? 1 : 0;
+                i++;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Returns a number of punctuation marks in a string.
+        /// </summary>
+        /// <param name="str">A <see cref="string"/> to search.</param>
+        /// <returns>A number of punctuation marks in a string.</returns>
+        public static int GetPunctuationCount(string str)
+        {
+            if (str is null)
+            {
+                throw new ArgumentNullException(nameof(str));
+            }
+
+            if (string.IsNullOrEmpty(str))
+            {
+                return 0;
+            }
+
+            int result = 0;
+            int i = 0;
+            while (i <= str.Length - 1)
+            {
+                bool isPunctuation = char.IsPunctuation(str[i]);
+                result += isPunctuation ? 1 : 0;
+                i++;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Returns a number of white space characters in a string.
+        /// </summary>
+        /// <param name="str">A <see cref="string"/> to search.</param>
+        /// <returns>A number of white space characters in a string.</returns>
+        public static int GetSpaceCountRecursive(string str)
+        {
+            if (str is null)
+            {
+                throw new ArgumentNullException(nameof(str));
+            }
+
+            if (string.IsNullOrEmpty(str))
+            {
+                return 0;
+            }
+
+            int result = GetSpaceCountRecursive(str[1..]) + (char.IsWhiteSpace(str[0]) ? 1 : 0);
+
+            return result;
+        }
+
+        /// <summary>
+        /// Returns a number of punctuation marks in a string.
+        /// </summary>
+        /// <param name="str">A <see cref="string"/> to search.</param>
+        /// <returns>A number of punctuation marks in a string.</returns>
+        public static int GetPunctuationCountRecursive(string str)
+        {
+            if (str is null)
+            {
+                throw new ArgumentNullException(nameof(str));
+            }
+
+            if (string.IsNullOrEmpty(str))
+            {
+                return 0;
+            }
+
+            bool isPunctuation = char.IsPunctuation(str[0]);
+            int currentIncrement = isPunctuation ? 1 : 0;
+            int result = GetPunctuationCountRecursive(str[1..]) + currentIncrement;
+
+            return result;
+        }
+    }
+}
